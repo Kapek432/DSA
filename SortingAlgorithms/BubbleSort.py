9603b225dbf69553b49075dbdaaf0c1596b999fe
Kacper Lipiec <peter.lipiec@poczta.onet.pl>
2025-04-01 18:21:52 +0200

    Delete SortingAlgorithms directory

--- SortingAlgorithms/BubbleSort.py
+++
@@ -1,7 +0,0 @@
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        for j in range(n-1-i):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-
